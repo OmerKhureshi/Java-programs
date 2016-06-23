d12e3f2d24f6927b601fd40580b3318c1669a840
Omer <syedomersalar@gmail.com>
2016-06-23 00:44:21 -0700

    Adding check if string can form palindrome problem

--- Palindrome/src/com/omer/Palindrome/Palindrom.java
+++ Palindrome/src/com/omer/Palindrome/Palindrom.java
@@ -1,39 +1,70 @@
 package com.omer.Palindrome;
-import java.lang.reflect.Array;
 
-public class Palindrom{
+import java.lang.reflect.Array;
+import java.util.BitSet;
+
+public class Palindrom {
 
 	public static void main(String[] args) {
 		Palindrom helloWorld = new Palindrom();
-		System.out.println(helloWorld.isPalindrome(2));
-		System.out.println(helloWorld.isPalindromeWithoutExtraSpace(1233321));
+		 System.out.println(helloWorld.isPalindrome(2));
+		 System.out.println(helloWorld.isPalindromeWithoutExtraSpace(1233321));
+		System.out.println(helloWorld.isThisStringAPalindrome("acaetaca"));
 	}
-	
-    public boolean isPalindrome(int x) {
-    	if (x < 0) return false;
-    	else if (x == 0) return true;
-        char[] number = String.valueOf(x).toCharArray();
-        int len = Array.getLength(number);
-        for(int i=0; i<=len/2; i++) {
-            if (number[i] != number[len-1-i]) 
-            	return false;
-        }
-        return true;
-    }
-    
-    public boolean isPalindromeWithoutExtraSpace(int x) {
-    	int pow = (int) (Math.log10(x));
-    	int len = (int) Math.pow(10, pow);
-    	int rem = 10;
-    	if (x < 0) return false;
-    	else if (x == 0 ) return true;
-    	while (x > 0) {
-    		//Compare left most and right most digits. x = 1221
-        	if (x/len != x%rem) 
-        		return false;
-        	x = (x % len) /10;
-        	len /= 100;    		
-    	}
-    	return true;
-    }
+
+	public boolean isPalindrome(int x) {
+		if (x < 0)
+			return false;
+		else if (x == 0)
+			return true;
+		char[] number = String.valueOf(x).toCharArray();
+		int len = Array.getLength(number);
+		for (int i = 0; i <= len / 2; i++) {
+			if (number[i] != number[len - 1 - i])
+				return false;
+		}
+		return true;
+	}
+
+	public boolean isPalindromeWithoutExtraSpace(int x) {
+		int pow = (int) (Math.log10(x));
+		int len = (int) Math.pow(10, pow);
+		int rem = 10;
+		if (x < 0)
+			return false;
+		else if (x == 0)
+			return true;
+		while (x > 0) {
+			// Compare left most and right most digits. x = 1221
+			if (x / len != x % rem)
+				return false;
+			x = (x % len) / 10;
+			len /= 100;
+		}
+		return true;
+	}
+
+	/**
+	 * This method checks if the string passed as argument can form a valid
+	 * palindrome.
+	 * 
+	 * @param str
+	 *            input string
+	 * @return
+	 */
+	public boolean isThisStringAPalindrome(String str) {
+		String[] src = str.split("");
+		BitSet alphabetBitSet = new BitSet(26);
+		for (int i = 0; i < src.length; i++) {
+			alphabetBitSet.flip(Character.toUpperCase(src[i].charAt(0)) - 65);
+		}
+
+		if (alphabetBitSet.cardinality() == 0)
+			return true;
+		else if (alphabetBitSet.cardinality() == 1)
+			return true;
+		else
+			return false;
+
+	}
 }
--- Palindrome/tests/com/omer/junit/UnitTests.java
+++ Palindrome/tests/com/omer/junit/UnitTests.java
@@ -10,6 +10,7 @@ import com.omer.Palindrome.Palindrom;
 
 public class UnitTests {
 
+	static boolean failed = false;
 	@BeforeClass
 	public static void beforeClass() {
 		System.out.println("Starting tests.");
@@ -17,23 +18,43 @@ public class UnitTests {
 	
 	@Test
 	public void testIsPalindrome() {
-		Palindrom tester = new Palindrom();
-		assertEquals("0 is a palindrome", true, tester.isPalindrome(0));
-		assertEquals("-1 is not a palindrome", false, tester.isPalindrome(-1));
-		assertEquals("123321 is a palindrome", true, tester.isPalindrome(123321));
+		try {
+			Palindrom tester = new Palindrom();
+			assertEquals("0 is a palindrome", true, tester.isPalindrome(0));
+			assertEquals("-1 is not a palindrome", false, tester.isPalindrome(-1));
+			assertEquals("123321 is a palindrome", true, tester.isPalindrome(123321));
+		} catch (AssertionError e) {
+			failed = true;
+		}
 	}
 	
 	@Test
 	public void testIsPalindromeWithoutExtraSpace() {
-		Palindrom tester = new Palindrom();
-		assertEquals("0 is a palindrome", true, tester.isPalindromeWithoutExtraSpace(0));
-		assertEquals("-1 is not a palindrome", false, tester.isPalindromeWithoutExtraSpace(-1));
-		assertEquals("123321 is a palindrome", true, tester.isPalindromeWithoutExtraSpace(123321));
+		try {
+			Palindrom tester = new Palindrom();
+			assertEquals("0 is a palindrome", true, tester.isPalindromeWithoutExtraSpace(0));
+			assertEquals("-1 is not a palindrome", false, tester.isPalindromeWithoutExtraSpace(-1));
+			assertEquals("123321 is a palindrome", true, tester.isPalindromeWithoutExtraSpace(123321));
+		} catch (AssertionError e) {
+			failed = true;
+		}
+	}
+
+	public void testIsThisStringAPalindrome() {
+		try {
+			Palindrom tester = new Palindrom();
+			assertEquals("aabbaa is a valid palindrome", true, tester.isThisStringAPalindrome("aabbaa"));
+			assertEquals("aabaa is a valid palindrome", false, tester.isThisStringAPalindrome("aabaa"));
+			assertEquals("aabbcc can form a valid palindrome", true, tester.isThisStringAPalindrome("aabbcc"));
+		} catch (AssertionError e) {
+			failed = true;
+		}
 	}
 	
 	@AfterClass
 	public static void afterClass() {
 		System.out.println("End of tests. All tests successful.");
+		if(!failed) System.out.println("All tests executed successfully.");
 	}
 	
 	
